507d39c6648e0beeae63436c734ca9baa229ebf2
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-09-03 13:31:54 +0100

    template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                'my_blue': '#4F66D1',
+                'my_gray': '#F7F5F7',
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 }
